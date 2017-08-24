55c3f78d49d056e7f48fb708f512249159ec6bcf
Hadi Fooladi Talari <hadi.f7@gmail.com>
2017-08-24 14:34:08 -0400

    Editor (v4.7.27) [SearchWindow Improvement]
    + New Option: Instant Search

--- Source/EasyConfig/Editor/Properties/Settings.Designer.cs
+++ Source/EasyConfig/Editor/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Editor.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -32,6 +32,18 @@ namespace Editor.Properties {
             }
             set {
                 this["ChangeLog_ShowNextTime"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool InstantSearch {
+            get {
+                return ((bool)(this["InstantSearch"]));
+            }
+            set {
+                this["InstantSearch"] = value;
             }
         }
     }
--- Source/EasyConfig/Editor/Windows/SearchWindow.xaml.cs
+++ Source/EasyConfig/Editor/Windows/SearchWindow.xaml.cs
@@ -3,28 +3,44 @@ using System.Collections.Generic;
 
 namespace Editor
 {
+	using Properties;
+
 	internal partial class SearchWindow
 	{
 		private SearchWindow()
 		{
 			InitializeComponent();
 
+			var D = Settings.Default;
+			cbInstant.IsChecked = D.InstantSearch;
+
+			// Event Handlers
 			Closing += (s, e) =>
 			{
 				Hide();
 				e.Cancel = true;
 			};
+
+			cbInstant.Checked += (s, e) => UpdateOptions();
+			cbInstant.Unchecked += (s, e) => UpdateOptions();
+		}
+
+		private void UpdateOptions()
+		{
+			var D = Settings.Default;
+			D.InstantSearch = cbInstant.IsChecked.isTrue();
+			D.Save();
 		}
 
 		private static SearchWindow ins;
 		public static SearchWindow Instance => ins ?? (ins = new SearchWindow());
 
-		#region Event Handlers
-		private void TB_SelectionChanged(object sender, RoutedEventArgs e)
+		private void Search()
 		{
 			string Word = TB.Text;
 			if (string.IsNullOrEmpty(Word))
 			{
+				lblSearch.Text = "";
 				DG.ItemsSource = null;
 				return;
 			}
@@ -36,6 +52,14 @@ namespace Editor
 				Items.Add(new Item(Result));
 
 			DG.ItemsSource = Items;
+			lblSearch.Text = $"Search Result for '{Word}'";
+		}
+
+		#region Event Handlers
+		private void TB_SelectionChanged(object sender, RoutedEventArgs e)
+		{
+			if (cbInstant.IsChecked.isTrue())
+				Search();
 		}
 
 		private void DG_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
@@ -44,7 +68,6 @@ namespace Editor
 			if (Item == null) return;
 
 			MainWindow.Instance.Reveal(Item.Result);
-
 		}
 		#endregion
 
@@ -70,5 +93,7 @@ namespace Editor
 				}
 			}
 		}
+
+		private void bSearch_Click(object sender, RoutedEventArgs e) => Search();
 	}
 }
