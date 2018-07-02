d14802f2f197d0a74a1b1b487983caa9622e299a
Hadi Fooladi Talari <hadi.f7@gmail.com>
2018-07-02 17:05:05 -0400

    New Project: Test

---
+++ Source/EasyConfig/Test/Config.cs
@@ -0,0 +1,32 @@
+﻿using EasyConfig.Attributes;
+using System.Collections.Generic;
+
+namespace Test
+{
+	internal class Config
+	{
+		public int Num;
+		public string Text;
+
+		[EasyConfig(Tag = "Person")]
+		public List<Person> Persons;
+
+		public override string ToString() => $"Num = {Num}, Text = {Text}, Persons = [{string.Join(", ", Persons ?? new List<Person>())}]";
+
+		public class Person
+		{
+			public int Age;
+			public string Name;
+
+			public Person() { }
+
+			public Person(string Name, int Age)
+			{
+				this.Age = Age;
+				this.Name = Name;
+			}
+
+			public override string ToString() => $"{Name} ({Age})";
+		}
+	}
+}
---
+++ Source/EasyConfig/Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Version VER = new Version(1, 0);
+			const string FILEPATH = "Test.xml";
+
+			var C = new Config
+			{
+				Num = 23,
+				Text = "EasyConfig",
+				Persons = new List<Config.Person>
+				{
+					new Config.Person("Hadi", 34),
+					new Config.Person("Ali", 78)
+				}
+			};
+
+			var EasyConfig = new EasyConfig.EasyConfig();
+
+			EasyConfig.Save(C, FILEPATH, "Config", VER);
+
+			var C2 = EasyConfig.Load<Config>(FILEPATH, VER);
+
+			Console.WriteLine(C2);
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+	}
+}
