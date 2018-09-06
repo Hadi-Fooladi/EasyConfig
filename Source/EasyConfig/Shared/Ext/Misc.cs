f86d88239522eedf6d5f11e1cc97568a384e9fae
Hadi Fooladi Talari <hadi.f7@gmail.com>
2018-09-06 10:37:57 -0400

    Moving Files

--- Source/EasyConfig/Shared/Ext/Misc.cs
+++ Source/EasyConfig/Shared/Ext/Misc.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace EasyConfig
 {
-	internal static class Ext
+	internal static class MiscExt
 	{
-		public static readonly Type CollectionType = typeof(ICollection);
+		private static readonly Type CollectionType = typeof(ICollection);
 
-		public static readonly Type[] GenericCollectionTypes =
+		private static readonly Type[] GenericCollectionTypes =
 		{
 			typeof(ICollection<>),
 			typeof(IReadOnlyCollection<>)
@@ -36,5 +37,9 @@ namespace EasyConfig
 		public static TValue GetValueOrNull<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> Dic, TKey Key)
 			where TValue : class
 			=> Dic.TryGetValue(Key, out var Value) ? Value : null;
+
+		public static bool HasAttribute<T>(this MemberInfo MI)
+			where T : Attribute
+			=> MI.GetCustomAttribute<T>() != null;
 	}
 }
