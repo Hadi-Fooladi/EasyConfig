62f2df1f0fb4b817f4f9b9eb54d4b87bdc7e33db
Hadi Fooladi Talari <hadi.f7@gmail.com>
2019-08-15 16:29:57 -0400

    Tests (v9.1.0.3)

---
+++ Source/EasyConfig/Tests/ListTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using EasyConfig.Attributes;
+
+using Org.XmlUnit.Diff;
+using Org.XmlUnit.Builder;
+
+namespace Tests
+{
+	[TestClass]
+	public class ListTests
+	{
+		private const string XML = "<Config><Item Value='A' /><Item Value='B' /><Item Value='C' /></Config>";
+
+		private static readonly XmlDocument Doc = XML.ToXmlDocument();
+
+		[TestMethod]
+		public void Load()
+		{
+			var C = Global.ecBasic.Load<Config>(Doc);
+
+			Assert.AreEqual(C.L.Count, 3);
+			Assert.AreEqual(C.L[0].Value, "A");
+			Assert.AreEqual(C.L[1].Value, "B");
+			Assert.AreEqual(C.L[2].Value, "C");
+		}
+
+		[TestMethod]
+		public void Save()
+		{
+			var C = new Config
+			{
+				L = new List<Item>
+				{
+					new Item { Value = "A" },
+					new Item { Value = "B" },
+					new Item { Value = "C" }
+				}
+			};
+
+			var D = DiffBuilder
+			   .Compare(Input.FromDocument(Doc))
+			   .WithTest(Input.FromDocument(Global.ecBasic.GetXmlDocument(C)))
+			   .CheckForIdentical()
+			   .WithComparisonController(ComparisonControllers.StopWhenDifferent)
+			   .Build();
+
+			Assert.IsFalse(D.HasDifferences());
+		}
+
+		private class Config
+		{
+			[Name("Item")]
+			public List<Item> L;
+		}
+
+		private class Item
+		{
+			public string Value;
+		}
+	}
+}
